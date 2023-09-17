330fc75ef495cc29f7f6d9f476651793fdbaa20a
Daniil <d.dubina@mobidev.biz>
2023-09-17 13:06:30 +0300

    phone number validation

---
+++ src/main/java/validPhoneNumber/ValidPhoneNumber.java
@@ -0,0 +1,24 @@
+package validPhoneNumber;
+
+import fileReader.FileReader;
+import java.io.*;
+import java.util.regex.*;
+
+public class ValidPhoneNumber {
+
+    FileReader fileReader = new FileReader();
+    String[] numbers;
+    StringBuilder result = new StringBuilder();
+
+    public StringBuilder validPhoneNumber() {
+        numbers = fileReader.readFile(new File("src/main/resources/file.txt")).split("\n");
+        Pattern pattern = Pattern.compile("^((\\(\\d{3}\\))|\\d{3})[- ]?\\d{3}-\\d{4}$");
+        for (final String number : numbers) {
+            Matcher matcher = pattern.matcher(number);
+            while (matcher.find()) {
+                result.append(matcher.group()).append("\n");
+            }
+        }
+        return result;
+    }
+}
